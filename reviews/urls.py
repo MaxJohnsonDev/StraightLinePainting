75406c16695ed8af94c1c851f33c74c51d92aa6a
Chiefautoparts <Chiefautoparts@outlook.com>
2021-05-16 20:55:37 -0700

    pushing recent changes

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,4 +6,5 @@ app_name = 'review'
 urlpatterns = [
     path('', views.review_list, name='review_list'),
     path('post', views.submitReview, name='submitReview'),
+    path('success', views.success, name='success'),
 ]
